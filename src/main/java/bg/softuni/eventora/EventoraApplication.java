038e0234326a84978e176544360c3ce6a7399a01
Student Developer <student@example.com>
2026-06-21 06:33:39 +0000

    feat: add spring boot application setup

---
+++ src/main/java/bg/softuni/eventora/EventoraApplication.java
@@ -0,0 +1,11 @@
+package bg.softuni.eventora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventoraApplication.class, args);
+    }
+}
